923a783ce4abb08ca74c1f385eae02c453dd178c
Jeff Korenstein <jeffrey.korenstein@va.gov>
2025-08-18 17:42:23 -0400

    Workitem VBMSD-000000: attempted mvn build and added Tutorial data model

---
+++ demo/src/main/java/com/example/demo/model/Tutorial.java
@@ -0,0 +1,31 @@
+package com.example.demo.model;
+import jakarta.persistence.*;
+@Entity
+@Table(name = "tutorials")
+public class Tutorial {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "tutorial_generator")
+    private long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "published")
+    private boolean published;
+
+    public Tutorial() {
+
+    }
+
+    public Tutorial(String title, String description, boolean published) {
+        this.title = title;
+        this.description = description;
+        this.published = published;
+    }
+
+    // getters and setters
+
+}
