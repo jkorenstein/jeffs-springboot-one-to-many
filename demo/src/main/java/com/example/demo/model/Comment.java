2bd81d615740fc8983b28dbaac31e40d39e7fa8a
Jeff Korenstein <jeffrey.korenstein@va.gov>
2025-08-19 14:04:15 -0400

    Workitem VBMSD-000000: added comment data model with relationships

---
+++ demo/src/main/java/com/example/demo/model/Comment.java
@@ -0,0 +1,26 @@
+package com.example.demo.model;
+import jakarta.persistence.*;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table(name = "comments")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "comment_generator")
+    private Long id;
+
+    @Lob
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "tutorial_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    private Tutorial tutorial;
+
+    // getters and setters
+}
